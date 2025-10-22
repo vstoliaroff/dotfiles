ade2773d5efe099b15d70f35598ad14636013cfc
Vincent Stoliaroff <24862807+vstoliaroff@users.noreply.github.com>
2025-10-22 07:15:53 +0000

    update snacks and disable which-key plugins

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,6 @@
 return {
 	'folke/snacks.nvim',
+	priority=1000,
 	lazy=false,
 	---@type snacks.Config
 	opts = {
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,11 +1,14 @@
-return {
-	'folke/which-key.nvim'
-	,event = 'VeryLazy'
-	,keys = {
-		'<leader>?'
-		,function() require('which-key').show({global=false}) end
-		,desc = "Buffer Local Keymaps (which-key)"
-		,
-	}
-	,
-}
+return { }
+
+
+-- return {
+-- 	'folke/which-key.nvim'
+-- 	,event = 'VeryLazy'
+-- 	,keys = {
+-- 		'<leader>?'
+-- 		,function() require('which-key').show({global=false}) end
+-- 		,desc = "Buffer Local Keymaps (which-key)"
+-- 		,
+-- 	}
+-- 	,
+-- }
