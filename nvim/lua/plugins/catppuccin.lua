24cb8a3a636ca74bdbcd0ffe69ebd4f6ffe53dcf
Vincent Stoliaroff <vstoliaroff@gmail.com>
2025-11-03 21:58:03 +0100

    Colorscheme config and lualine

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,3 +1,21 @@
 return {
-    'catppuccin/nvim',name = "catppuccin", priority = 1000
+	'catppuccin/nvim',
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			transparent_background = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				telescope = true,
+				which_key = true,
+			},
+		})
+		-- Apply the colorscheme
+		vim.cmd.colorscheme("catppuccin")
+	end,
 } 
